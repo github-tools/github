30891f76ce532dd8cd9988483637ad739463865e
ingalls <nick@mapbox.com>
2014-12-11 11:42:17 -0500

    Better auth check

--- test/test.auth.js
+++ test/test.auth.js
@@ -11,10 +11,30 @@ test("Basic Auth - Pass", function(t) {
     });
     var user = github.getUser();
     
-    t.equals(Object.keys(user).length, 10);
-    t.equals(Object.keys(user).indexOf('repos') > -1, true); 
+    user.notifications(function(err, res) {
+        t.error(err, 'user is authd');
+    });
+    
     clearTimeout(timeout);
     t.end(); 
     
 });
 
+test("Basic Auth - Fail", function(t) {
+    var timeout = setTimeout(function () { t.fail(); }, 10000);
+    var github = new Github({
+      username: test_user.USERNAME,
+      password: 'fake124',
+      auth: "basic"
+    });
+    var user = github.getUser();
+
+    user.notifications(function(err, res) {
+        t.ok(err, 'user is not authd');
+        t.equals(JSON.parse(err.request.responseText).message, 'Bad credentials', 'confirm error');
+    });
+
+    clearTimeout(timeout);
+    t.end();
+ 
+});
