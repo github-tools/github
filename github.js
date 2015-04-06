4e7b00f6474c6b57f0098a146c6196321aa5c362
Ændrew Rininsland <aendrew@aendrew.com>
2015-04-06 03:57:21 +0100

    Adding .jshintrc and linting everything.

--- github.js
+++ github.js
@@ -11,19 +11,23 @@
  */
 
 (function() {
-
+  'use strict';
+  
   // Initial Setup
   // -------------
 
   var XMLHttpRequest,  _;
+  /* istanbul ignore else  */
   if (typeof exports !== 'undefined') {
       XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
       _ = require('underscore');
-      btoa = require('btoa');
-  } else {
-      _ = window._;
+      var btoa = require('btoa'); //jshint ignore:line
+  } else { 
+      _ = window._; 
   }
+  
   //prefer native XMLHttpRequest always
+  /* istanbul ignore if  */
   if (typeof window !== 'undefined' && typeof window.XMLHttpRequest !== 'undefined'){
       XMLHttpRequest = window.XMLHttpRequest;
   }
@@ -41,7 +45,7 @@
     function _request(method, path, data, cb, raw, sync) {
       function getURL() {
         var url = path.indexOf('//') >= 0 ? path : API_URL + path;
-        return url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime();
+        return url + ((/\?/).test(url) ? '&' : '?') + (new Date()).getTime();
       }
 
       var xhr = new XMLHttpRequest();
@@ -50,7 +54,7 @@
       xhr.open(method, getURL(), !sync);
       if (!sync) {
         xhr.onreadystatechange = function () {
-          if (this.readyState == 4) {
+          if (this.readyState === 4) {
             if (this.status >= 200 && this.status < 300 || this.status === 304) {
               cb(null, raw ? this.responseText : this.responseText ? JSON.parse(this.responseText) : true, this);
             } else {
@@ -61,7 +65,7 @@
       }
 
       if (!raw) {
-        xhr.dataType = "json";
+        xhr.dataType = 'json';
         xhr.setRequestHeader('Accept','application/vnd.github.v3+json');
       } else {
         xhr.setRequestHeader('Accept','application/vnd.github.v3.raw+json');
@@ -72,17 +76,20 @@
         var authorization = options.token ? 'token ' + options.token : 'Basic ' + btoa(options.username + ':' + options.password);
         xhr.setRequestHeader('Authorization', authorization);
       }
-      if (data)
+      if (data) {
         xhr.send(JSON.stringify(data));
-      else
+      } else {
         xhr.send();
-      if (sync) return xhr.response;
+      }
+      if (sync) {
+        return xhr.response;
+      }
     }
 
     function _requestAllPages(path, cb) {
       var results = [];
       (function iterate() {
-        _request("GET", path, null, function(err, res, xhr) {
+        _request('GET', path, null, function(err, res, xhr) {
           if (err) {
             return cb(err);
           }
@@ -113,7 +120,7 @@
     Github.User = function() {
       this.repos = function(cb) {
         // Github does not always honor the 1000 limit so we want to iterate over the data set.
-        _requestAllPages("/user/repos?type=all&per_page=1000&sort=updated", function(err, res) {
+        _requestAllPages('/user/repos?type=all&per_page=1000&sort=updated', function(err, res) {
           cb(err, res);
         });
       };
@@ -122,7 +129,7 @@
       // -------
 
       this.orgs = function(cb) {
-        _request("GET", "/user/orgs", null, function(err, res) {
+        _request("GET", '/user/orgs', null, function(err, res) {
           cb(err, res);
         });
       };
@@ -131,7 +138,7 @@
       // -------
 
       this.gists = function(cb) {
-        _request("GET", "/gists", null, function(err, res) {
+        _request("GET", '/gists', null, function(err, res) {
           cb(err,res);
         });
       };
@@ -140,7 +147,7 @@
       // -------
 
       this.notifications = function(cb) {
-        _request("GET", "/notifications", null, function(err, res) {
+        _request("GET", '/notifications', null, function(err, res) {
           cb(err,res);
         });
       };
@@ -149,9 +156,9 @@
       // -------
 
       this.show = function(username, cb) {
-        var command = username ? "/users/"+username : "/user";
+        var command = username ? '/users/' + username : '/user';
 
-        _request("GET", command, null, function(err, res) {
+        _request('GET', command, null, function(err, res) {
           cb(err, res);
         });
       };
@@ -161,7 +168,7 @@
 
       this.userRepos = function(username, cb) {
         // Github does not always honor the 1000 limit so we want to iterate over the data set.
-        _requestAllPages("/users/"+username+"/repos?type=all&per_page=1000&sort=updated", function(err, res) {
+        _requestAllPages('/users/' + username + '/repos?type=all&per_page=1000&sort=updated', function(err, res) {
           cb(err, res);
         });
       };
@@ -170,7 +177,7 @@
       // -------
 
       this.userGists = function(username, cb) {
-        _request("GET", "/users/"+username+"/gists", null, function(err, res) {
+        _request('GET', '/users/' + username + '/gists', null, function(err, res) {
           cb(err,res);
         });
       };
@@ -180,7 +187,7 @@
 
       this.orgRepos = function(orgname, cb) {
         // Github does not always honor the 1000 limit so we want to iterate over the data set.
-        _requestAllPages("/orgs/"+orgname+"/repos?type=all&&page_num=1000&sort=updated&direction=desc", function(err, res) {
+        _requestAllPages('/orgs/' + orgname + '/repos?type=all&&page_num=1000&sort=updated&direction=desc', function(err, res) {
           cb(err, res);
         });
       };
@@ -189,7 +196,7 @@
       // -------
 
       this.follow = function(username, cb) {
-        _request("PUT", "/user/following/"+username, null, function(err, res) {
+        _request('PUT', '/user/following/' + username, null, function(err, res) {
           cb(err, res);
         });
       };
@@ -198,7 +205,7 @@
       // -------
 
       this.unfollow = function(username, cb) {
-        _request("DELETE", "/user/following/"+username, null, function(err, res) {
+        _request('DELETE', '/user/following/' + username, null, function(err, res) {
           cb(err, res);
         });
       };
@@ -206,7 +213,7 @@
       // Create a repo
       // -------
       this.createRepo = function(options, cb) {
-        _request("POST", "/user/repos", options, cb);
+        _request('POST', '/user/repos', options, cb);
       };
 
     };
@@ -219,11 +226,11 @@
       var user = options.user;
 
       var that = this;
-      var repoPath = "/repos/" + user + "/" + repo;
+      var repoPath = '/repos/' + user + '/' + repo;
 
       var currentTree = {
-        "branch": null,
-        "sha": null
+        'branch': null,
+        'sha': null
       };
 
 
@@ -231,15 +238,18 @@
       // --------
 
       this.deleteRepo = function(cb) {
-        _request("DELETE", repoPath, options, cb);
+        _request('DELETE', repoPath, options, cb);
       };
 
       // Uses the cache if branch has not been changed
       // -------
 
       function updateTree(branch, cb) {
-        if (branch === currentTree.branch && currentTree.sha) return cb(null, currentTree.sha);
-        that.getRef("heads/"+branch, function(err, sha) {
+        if (branch === currentTree.branch && currentTree.sha) {
+          return cb(null, currentTree.sha);
+        }
+        
+        that.getRef('heads/' + branch, function(err, sha) {
           currentTree.branch = branch;
           currentTree.sha = sha;
           cb(err, sha);
@@ -250,8 +260,11 @@
       // -------
 
       this.getRef = function(ref, cb) {
-        _request("GET", repoPath + "/git/refs/" + ref, null, function(err, res) {
-          if (err) return cb(err);
+        _request('GET', repoPath + '/git/refs/' + ref, null, function(err, res) {
+          if (err) {
+            return cb(err);
+          }
+          
           cb(null, res.object.sha);
         });
       };
@@ -265,7 +278,7 @@
       // }
 
       this.createRef = function(options, cb) {
-        _request("POST", repoPath + "/git/refs", options, cb);
+        _request('POST', repoPath + '/git/refs', options, cb);
       };
 
       // Delete a reference
@@ -275,29 +288,32 @@
       // repo.deleteRef('tags/v1.0')
 
       this.deleteRef = function(ref, cb) {
-        _request("DELETE", repoPath + "/git/refs/"+ref, options, cb);
+        _request('DELETE', repoPath + '/git/refs/' + ref, options, cb);
       };
 
       // Create a repo
       // -------
 
       this.createRepo = function(options, cb) {
-        _request("POST", "/user/repos", options, cb);
+        _request('POST', '/user/repos', options, cb);
       };
 
       // Delete a repo
       // --------
 
       this.deleteRepo = function(cb) {
-        _request("DELETE", repoPath, options, cb);
+        _request('DELETE', repoPath, options, cb);
       };
 
       // List all tags of a repository
       // -------
 
       this.listTags = function(cb) {
-        _request("GET", repoPath + "/tags", null, function(err, tags) {
-          if (err) return cb(err);
+        _request('GET', repoPath + '/tags', null, function(err, tags) {
+          if (err) {
+            return cb(err);
+          }
+          
           cb(null, tags);
         });
       };
@@ -306,7 +322,7 @@
       // -------
 
       this.listPulls = function(state, cb) {
-        _request("GET", repoPath + "/pulls" + (state ? '?state=' + state : ''), null, function(err, pulls) {
+        _request('GET', repoPath + "/pulls" + (state ? '?state=' + state : ''), null, function(err, pulls) {
           if (err) return cb(err);
           cb(null, pulls);
         });
@@ -465,7 +481,7 @@
       // -------
 
       this.updateHead = function(head, commit, cb) {
-        _request("PATCH", repoPath + "/git/refs/heads/" + head, { "sha": commit }, function(err, res) {
+        _request("PATCH", repoPath + "/git/refs/heads/" + head, { "sha": commit }, function(err) {
           cb(err);
         });
       };
@@ -624,7 +640,7 @@
 
       this.write = function(branch, path, content, message, cb) {
         that.getSha(branch, path, function(err, sha) {
-          if (err && err.error!=404) return cb(err);
+          if (err && err.error !== 404) return cb(err);
           _request("PUT", repoPath + "/contents/" + path, {
             message: message,
             content: btoa(content),
@@ -774,7 +790,9 @@
       this.list = function(options, cb) {
         var query = [];
         for (var key in options) {
-          query.push(encodeURIComponent(key) + "=" + encodeURIComponent(options[key]));
+          if (options.hasOwnProperty(key)) {
+            query.push(encodeURIComponent(key) + "=" + encodeURIComponent(options[key]));
+          }
         }
         _requestAllPages(path + '?' + query.join("&"), cb);
       };
@@ -800,9 +818,8 @@
     };
   };
 
-
+  /* istanbul ignore else  */
   if (typeof exports !== 'undefined') {
-    // Github = exports;
     module.exports = Github;
   } else {
     window.Github = Github;
--- test/test.repo.js
+++ test/test.repo.js
@@ -1,4 +1,6 @@
-var test = require('tape');
+'use strict';
+
+var test = require('tape'); //jshint ignore:line
 var Github = require("../");
 var test_user = require('./user.json');
 
@@ -20,14 +22,14 @@ test("Repo API", function(t) {
     });
 
     t.test('repo.contents', function(q) {
-        repo.contents('master', './', function(err, res) {
+        repo.contents('master', './', function(err) {
             q.error(err, 'get repo contents');
             q.end();
         });
     });
 
     t.test('repo.fork', function(q) {
-        repo.fork(function(err, res) {
+        repo.fork(function(err) {
             q.error(err, 'test fork repo');
             q.end();
         });
@@ -36,7 +38,7 @@ test("Repo API", function(t) {
     //@TODO repo.branch, repo.pull
 
     t.test('repo.listBranches', function(q) {
-        repo.listBranches(function(err, res) {
+        repo.listBranches(function(err) {
             q.error(err, 'list branches');
             q.end();
         });
@@ -66,66 +68,64 @@ test("Repo API", function(t) {
 var repoTest = Date.now();
 
 test('Create Repo', function(t) {
-    var timeout = setTimeout(function () { t.fail(); }, 10000);
-    var github = new Github({
-        username: test_user.USERNAME,
-        password: test_user.PASSWORD,
-        auth: "basic"
-    });
-    var user = github.getUser();
+  var timeout = setTimeout(function () { t.fail(); }, 10000);
+  var github = new Github({
+    username: test_user.USERNAME,
+    password: test_user.PASSWORD,
+    auth: "basic"
+  });
+  var user = github.getUser();
 
-    t.test('user.createRepo', function(q) {
-        user.createRepo({ "name": repoTest }, function (err, res) {
-            q.error(err);
-            q.equals(res.name, repoTest.toString(), 'Repo created');
-            q.end();
-        });
+  t.test('user.createRepo', function(q) {
+    user.createRepo({ "name": repoTest }, function (err, res) {
+      q.error(err);
+      q.equals(res.name, repoTest.toString(), 'Repo created');
+      q.end();
     });
-    var repo = github.getRepo(test_user.USERNAME, repoTest);
+  });
+  var repo = github.getRepo(test_user.USERNAME, repoTest);
 
-    t.test('repo.write', function(q) {
-        repo.write('master', 'TEST.md', 'THIS IS A TEST', 'Creating test', function(err) {
-            q.error(err);
-            q.end();
-        });
+  t.test('repo.write', function(q) {
+    repo.write('master', 'TEST.md', 'THIS IS A TEST', 'Creating test', function(err) {
+      q.error(err);
+      q.end();
     });
-    clearTimeout(timeout);
-    t.end();
+  });
+  clearTimeout(timeout);
+  t.end();
 });
 
 test('delete Repo', function(t) {
-    var timeout = setTimeout(function () { t.fail(); }, 10000);
-    var github = new Github({
-        username: test_user.USERNAME,
-        password: test_user.PASSWORD,
-        auth: "basic"
-    });
-    var repo = github.getRepo(test_user.USERNAME, repoTest);
-    repo.deleteRepo(function(err, res) {
-        t.error(err);
-        t.equals(res, true, 'Repo Deleted');
-        clearTimeout(timeout);
-        t.end();
-    });
-
+  var timeout = setTimeout(function () { t.fail(); }, 10000);
+  var github = new Github({
+    username: test_user.USERNAME,
+    password: test_user.PASSWORD,
+    auth: "basic"
+  });
+  var repo = github.getRepo(test_user.USERNAME, repoTest);
+  
+  repo.deleteRepo(function(err, res) {
+    t.error(err);
+    t.equals(res, true, 'Repo Deleted');
+    clearTimeout(timeout);
+    t.end();
+  });
 });
 
 test('Repo Returns commit errors correctly', function(t) {
-    var timeout = setTimeout(function () { t.fail(); }, 10000);
-    var github = new Github({
-      username: test_user.USERNAME,
-      password: test_user.PASSWORD,
-      auth: "basic"
-    });
-    var repo = github.getRepo(test_user.USERNAME, test_user.REPO);
+  var timeout = setTimeout(function () { t.fail(); }, 10000);
+  var github = new Github({
+    username: test_user.USERNAME,
+    password: test_user.PASSWORD,
+    auth: "basic"
+  });
+  var repo = github.getRepo(test_user.USERNAME, test_user.REPO);
 
-    repo.commit("broken-parent-hash", "broken-tree-hash", "commit message", function(err){
-        t.ok(err, 'error thrown for bad commit');
-        t.ok(err.request);
-        t.equals(err.request.status, 422, 'Returns 422 status');
-        clearTimeout(timeout);
-        t.end();
-    });
-
-
+  repo.commit("broken-parent-hash", "broken-tree-hash", "commit message", function(err){
+    t.ok(err, 'error thrown for bad commit');
+    t.ok(err.request);
+    t.equals(err.request.status, 422, 'Returns 422 status');
+    clearTimeout(timeout);
+    t.end();
+  });
 });
