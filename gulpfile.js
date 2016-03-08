41782f3c253375e6f7148333c96a73e791f88fa7
Clay Reimann <clayreimann@gmail.com>
2016-03-08 10:00:43 -0600

    Add code coverage back to testing

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@
 var gulp = require('gulp');
 
 var babel = require('gulp-babel');
+var istanbul = require('gulp-istanbul');
 var jscs = require('gulp-jscs');
 var jshint = require('gulp-jshint');
+
 var mocha = require('gulp-mocha');
 var rename = require('gulp-rename');
 var stylish = require('gulp-jscs-stylish');
@@ -13,89 +15,39 @@ var browserify = require('browserify');
 var buffer = require('vinyl-buffer');
 var del = require('del');
 var path = require('path');
-var karma = require('karma');
+var Promise = require('es6-promise').Promise;
 var source = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 
-function runTests(singleRun, isCI, done) {
-   var reporters = ['mocha'];
-   var preprocessors = {};
-
-   var files = [
-      path.join(__dirname, 'test/vendor/*.js'), // PhantomJS 1.x polyfills
-      path.join(__dirname, 'test/test.*.js')
-   ];
-
-   if (singleRun) {
-      preprocessors['test/test.*.js'] = ['browserify'];
-      reporters.push('coverage');
-   }
-
-   files.push({
-      pattern: path.join(__dirname, 'test/gh.png'),
-      watched: false,
-      included: false
-   });
-
-   var localConfig = {
-      files: files,
-      configFile: path.join(__dirname, './karma.conf.js'),
-      singleRun: singleRun,
-      autoWatch: !singleRun,
-      reporters: reporters,
-      preprocessors: preprocessors
-   };
-
-   if (isCI) {
-      localConfig.sauceLabs = {
-         testName: 'GitHub.js UAT tests',
-         idleTimeout: 120000,
-         recordVideo: false
-      };
-
-      // Increase timeouts massively so Karma doesn't timeout in Sauce tunnel.
-      localConfig.browserNoActivityTimeout = 400000;
-      localConfig.captureTimeout = 120000;
-      localConfig.customLaunchers = sauceLaunchers;
-      localConfig.browsers = Object.keys(sauceLaunchers);
-      reporters.push('saucelabs');
-
-      // Set Mocha timeouts to longer.
-      localConfig.client = {
-         mocha: {
-            timeout: 20000
-         }
-      };
-   }
-
-   var server = new karma.Server(localConfig, function(failCount) {
-      done(failCount ? new Error('Failed ' + failCount + ' tests.') : null);
-   });
-
-   server.start();
-} // End runTests()
-
 gulp.task('lint', function() {
-   return gulp.src([
+   var sources = [
       path.join(__dirname, '/*.js'),
       path.join(__dirname, '/src/*.js'),
       path.join(__dirname, '/test/*.js')
-   ],
-   {
+   ];
+   var opts = {
       base: './'
-   })
-   .pipe(jshint())
-   .pipe(jscs({
-      fix: true
-   }))
-   .pipe(stylish.combineWithHintResults())
-   .pipe(jscs.reporter())
-   .pipe(jshint.reporter('jshint-stylish'))
-   .pipe(gulp.dest('.'));
+   };
+
+   return gulp.src(sources, opts)
+      .pipe(jshint())
+      .pipe(jscs())
+      .pipe(stylish.combineWithHintResults())
+      .pipe(jscs.reporter())
+      .pipe(jshint.reporter('jshint-stylish'))
+      .pipe(gulp.dest('.'))
+      ;
 });
 
-gulp.task('test:mocha', function() {
+gulp.task('coverage', function() {
+   return gulp.src('src/**/*.js')
+      .pipe(istanbul())
+      .pipe(istanbul.hookRequire())
+      ;
+});
+
+gulp.task('test:mocha', ['coverage'], function() {
    var srcOpts = {
       read: false
    };
@@ -106,15 +58,12 @@ gulp.task('test:mocha', function() {
 
    return gulp.src('test/test.*.js', srcOpts)
       .pipe(mocha(mochaOpts))
+      .pipe(istanbul.writeReports())
       ;
 });
 
-gulp.task('test:browser', function(done) {
-   runTests(true, true, done);
-});
-
-gulp.task('clean', function () {
-   return del('dist/*');
+gulp.task('clean', function() {
+   return Promise.all([del('dist/*'), del('coverage/*')]);
 });
 
 gulp.task('build', function() {
@@ -140,17 +89,15 @@ gulp.task('build', function() {
       .pipe(gulp.dest('dist'))
       ;
 
-   var babeled = gulp.src('src/github.js')
+   gulp.src('src/github.js')
       .pipe(babel())
-      ;
-
-   babeled
       .pipe(sourcemaps.init())
       .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest('dist'))
       ;
 
-   return babeled
+   return gulp.src('src/github.js')
+      .pipe(babel())
       .pipe(sourcemaps.init())
       .pipe(rename({
          extname: '.min.js'
@@ -162,48 +109,5 @@ gulp.task('build', function() {
 });
 
 gulp.task('default', ['clean'], function() {
-   gulp.start('lint', 'test', 'build');
+   gulp.start('lint', 'test:mocha', 'build');
 });
-
-var sauceLaunchers = {
-   SL_Chrome: {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      version: '45'
-   },
-   SL_Firefox: {
-      base: 'SauceLabs',
-      browserName: 'firefox',
-      version: '39'
-   },
-   SL_Safari: {
-      base: 'SauceLabs',
-      browserName: 'safari',
-      platform: 'OS X 10.10',
-      version: '8'
-   },
-   SL_IE_9: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 7',
-      version: '9'
-   },
-   SL_IE_10: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 2012',
-      version: '10'
-   },
-   SL_IE_11: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11'
-   },
-   SL_iOS: {
-      base: 'SauceLabs',
-      browserName: 'iphone',
-      platform: 'OS X 10.10',
-      version: '8.1'
-   }
-};
