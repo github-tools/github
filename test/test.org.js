e278c5a076e77f1882bf7df03fcf4ecff639d4e6
marcosomma <mak.so1979@gmail.com>
2016-03-12 13:51:19 +0100

    fix npm run lint issue && removed quote from object && replaced double quete with single quote for Strings

--- test/test.org.js
+++ test/test.org.js
@@ -16,24 +16,24 @@ describe('Github.Organization', function() {
 
    it('should create an organisation repo', function(done) {
       var repoTest = Math.floor(Math.random() * (100000 - 0)) + 0;
-      var options ={
-          "orgname": testUser.ORGANIZATION,
-          "name": repoTest,
-          "description": "test create organization repo",
-          "homepage": "https://github.com/",
-          "private": false,
-          "has_issues": true,
-          "has_wiki": true,
-          "has_downloads": true
+      var options = {
+         orgname: testUser.ORGANIZATION,
+         name: repoTest,
+         description: 'test create organization repo',
+         homepage: 'https://github.com/',
+         private: false,
+         has_issues: true,
+         has_wiki: true,
+         has_downloads: true
       };
+
       organization.createRepo(options, function(err, res, xhr) {
          should.not.exist(err);
          xhr.should.be.instanceof(XMLHttpRequest);
          res.name.should.equal(repoTest.toString());
-         res.full_name.should.equal(testUser.ORGANIZATION+"/"+repoTest.toString());
+         res.full_name.should.equal(testUser.ORGANIZATION + '/' + repoTest.toString());
 
          done();
       });
    });
-
 });
