eb2b4f311c2d5f876bcc669c2b78092847e9f9b7
Mahmudul Hazra <mahme11@gmail.com>
2019-09-19 21:30:55 +0200

    fix(repository): prevents lib from crashing when not providing optional arguments

--- test/repository.spec.js
+++ test/repository.spec.js
@@ -385,6 +385,17 @@ describe('Repository', function() {
          }));
       });
 
+      it('should successfully write to repo when not providing optional options argument', function(done) {
+         remoteRepo.writeFile('master', fileName, initialText, initialMessage, undefined, assertSuccessful(done, function() {
+            wait()().then(() => remoteRepo.getContents('master', fileName, 'raw',
+               assertSuccessful(done, function(err, fileText) {
+                  expect(fileText).to.be(initialText);
+
+                  done();
+               })));
+         }));
+      });
+
       it('should rename files', function(done) {
          remoteRepo.writeFile('master', fileName, initialText, initialMessage, assertSuccessful(done, function() {
             wait()().then(() => remoteRepo.move('master', fileName, 'new_name', assertSuccessful(done, function() {
